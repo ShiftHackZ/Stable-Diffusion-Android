8ec86d14370fb5447157f5a84406aa9186f99518
ShiftHackZ <dmitriy@moroz.cc>
2023-03-09 19:12:43 +0200

    New flavor

--- presentation/src/main/java/com/shifthackz/aisdv1/presentation/screen/settings/SettingsContract.kt
+++ presentation/src/main/java/com/shifthackz/aisdv1/presentation/screen/settings/SettingsContract.kt
@@ -3,10 +3,26 @@ package com.shifthackz.aisdv1.presentation.screen.settings
 import com.shifthackz.aisdv1.core.ui.MviState
 
 interface SettingsState : MviState {
-    object Uninitialized : SettingsState
+
+    val screenDialog: Dialog
+
+    object Uninitialized : SettingsState {
+        override val screenDialog = Dialog.None
+    }
 
     data class Content(
+        override val screenDialog: Dialog = Dialog.None,
         val sdModels: List<String>,
         val sdModelSelected: String,
     ) : SettingsState
+
+    fun withDialog(value: Dialog): SettingsState = when (this) {
+        is Content -> copy(screenDialog = value)
+        else -> this
+    }
+
+    sealed interface Dialog {
+        object None : Dialog
+        object Communicating : Dialog
+    }
 }
