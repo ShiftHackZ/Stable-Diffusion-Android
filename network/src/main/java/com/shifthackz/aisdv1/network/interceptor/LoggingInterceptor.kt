8d0ec0d7b3e7202d568ad39adb5c046103faeaa1
ShiftHackZ <dmitriy@moroz.cc>
2023-03-26 23:22:45 +0300

    Custom sdai cloud patch 6

--- network/src/main/java/com/shifthackz/aisdv1/network/interceptor/LoggingInterceptor.kt
+++ network/src/main/java/com/shifthackz/aisdv1/network/interceptor/LoggingInterceptor.kt
@@ -1,19 +1,16 @@
 package com.shifthackz.aisdv1.network.interceptor
 
-import com.shifthackz.aisdv1.core.common.appbuild.BuildInfoProvider
 import com.shifthackz.aisdv1.core.common.log.debugLog
 import com.shifthackz.aisdv1.network.qualifiers.ApiUrlProvider
 import okhttp3.logging.HttpLoggingInterceptor
 
 class LoggingInterceptor(
-    private val buildInfoProvider: BuildInfoProvider,
     private val apiUrlProvider: ApiUrlProvider,
 ) {
 
     fun get() = HttpLoggingInterceptor { message ->
         val badPredicate = message.contains(apiUrlProvider.stableDiffusionCloudAiApiUrl)
-//        if (!badPredicate)
-            debugLog(HTTP_TAG, message)
+        if (!badPredicate) debugLog(HTTP_TAG, message)
     }.apply {
         level = HttpLoggingInterceptor.Level.BODY
     }
