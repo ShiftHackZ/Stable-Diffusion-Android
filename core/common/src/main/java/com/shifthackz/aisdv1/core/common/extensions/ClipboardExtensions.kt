34f0243456847c20ed04bc399859cc23aa5d42ee
ShiftHackZ <41302477+ShiftHackZ@users.noreply.github.com>
2023-07-24 06:55:33 +0000

    Update ClipboardExtensions.kt

--- core/common/src/main/java/com/shifthackz/aisdv1/core/common/extensions/ClipboardExtensions.kt
+++ core/common/src/main/java/com/shifthackz/aisdv1/core/common/extensions/ClipboardExtensions.kt
@@ -4,7 +4,7 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 
-fun Context.copyToClipboard(text: CharSequence){
+fun Context.copyToClipboard(text: CharSequence) {
     val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
     val clip = ClipData.newPlainText("label", text)
     clipboard.setPrimaryClip(clip)
