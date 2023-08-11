46ab5b18c23fe40e61d21bcf7880e88f11fabeff
ShiftHackZ <dmitriy@moroz.cc>
2023-08-11 12:39:27 +0300

    Migrate to Applovin :: Patch 2

--- feature/ads/src/main/java/com/shifthackz/aisdv1/feature/ads/di/AdFeatureModule.kt
+++ feature/ads/src/main/java/com/shifthackz/aisdv1/feature/ads/di/AdFeatureModule.kt
@@ -2,10 +2,10 @@ package com.shifthackz.aisdv1.feature.ads.di
 
 import com.shifthackz.aisdv1.domain.feature.ad.AdFeature
 import com.shifthackz.aisdv1.feature.ads.AdFeatureImpl
-import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val adFeatureOldModule = module {
-    factoryOf(::AdFeatureImpl) bind AdFeature::class
+    singleOf(::AdFeatureImpl) bind AdFeature::class
 }
