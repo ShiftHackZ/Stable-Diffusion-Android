c693798738a352820b6a3bcd7c1537231e24344f
ShiftHackZ <41302477+ShiftHackZ@users.noreply.github.com>
2024-02-08 12:58:27 +0200

    Hotfix Verifier Rejected class V2 (#120)

--- presentation/src/main/java/com/shifthackz/aisdv1/presentation/screen/setup/ServerSetupScreen.kt
+++ presentation/src/main/java/com/shifthackz/aisdv1/presentation/screen/setup/ServerSetupScreen.kt
@@ -5,10 +5,12 @@ package com.shifthackz.aisdv1.presentation.screen.setup
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -28,12 +30,17 @@ import androidx.compose.material.icons.filled.Help
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
 import androidx.compose.material.icons.outlined.ArrowBack
+import androidx.compose.material.icons.outlined.FileDownload
+import androidx.compose.material.icons.outlined.FileDownloadDone
+import androidx.compose.material.icons.outlined.FileDownloadOff
+import androidx.compose.material.icons.outlined.Landslide
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedButton
@@ -44,6 +51,7 @@ import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -54,6 +62,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.times
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.shifthackz.aisdv1.core.common.appbuild.BuildInfoProvider
 import com.shifthackz.aisdv1.core.common.appbuild.BuildType
@@ -61,13 +70,13 @@ import com.shifthackz.aisdv1.core.common.links.LinksProvider
 import com.shifthackz.aisdv1.core.model.asString
 import com.shifthackz.aisdv1.core.model.asUiText
 import com.shifthackz.aisdv1.core.ui.MviScreen
+import com.shifthackz.aisdv1.domain.entity.DownloadState
 import com.shifthackz.aisdv1.domain.entity.LocalAiModel
 import com.shifthackz.aisdv1.presentation.R
 import com.shifthackz.aisdv1.presentation.utils.Constants
 import com.shifthackz.aisdv1.presentation.widget.dialog.ErrorDialog
 import com.shifthackz.aisdv1.presentation.widget.dialog.ProgressDialog
 import com.shifthackz.aisdv1.presentation.widget.input.DropdownTextField
-import com.shifthackz.aisdv1.presentation.widget.item.LocalModelItemComposable
 import com.shifthackz.aisdv1.presentation.widget.item.SettingsItem
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
@@ -526,7 +535,7 @@ private fun LocalDiffusionSetupTab(
                 if (state.localCustomModel) customPredicate else !customPredicate
             }
             .forEach { localModel ->
-                LocalModelItemComposable(
+                LdModelComposable(
                     model = localModel,
                     onDownloadCardButtonClick = onDownloadCardButtonClick,
                     onSelect = onSelectLocalModel,
@@ -585,6 +594,192 @@ private fun ConfigurationModeButton(
             textAlign = TextAlign.Center,
             lineHeight = 15.sp,
         )
+    }
+}
+
+@Composable
+fun LdModelComposable(
+    modifier: Modifier = Modifier,
+    model: ServerSetupState.LocalModel,
+    onDownloadCardButtonClick: (ServerSetupState.LocalModel) -> Unit = {},
+    onSelect: (ServerSetupState.LocalModel) -> Unit = {}
+) {
+    Column(
+        modifier = modifier
+            .padding(vertical = 8.dp)
+            .fillMaxWidth()
+            .clip(RoundedCornerShape(16.dp))
+            .background(color = MaterialTheme.colorScheme.surfaceTint.copy(alpha = 0.8f))
+            .defaultMinSize(minHeight = 50.dp)
+            .border(
+                width = 2.dp,
+                shape = RoundedCornerShape(16.dp),
+                color = if (model.selected) MaterialTheme.colorScheme.primary else Color.Transparent,
+            )
+            .clickable { onSelect(model) },
+    ) {
+        Row(
+            modifier = Modifier.padding(vertical = 4.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            val icon = when (model.downloadState) {
+                is DownloadState.Downloading -> Icons.Outlined.FileDownload
+                else -> when {
+                    model.id == LocalAiModel.CUSTOM.id -> Icons.Outlined.Landslide
+                    model.downloaded -> Icons.Outlined.FileDownloadDone
+                    else -> Icons.Outlined.FileDownloadOff
+                }
+            }
+            Icon(
+                modifier = modifier
+                    .padding(horizontal = 8.dp)
+                    .size(48.dp),
+                imageVector = icon,
+                contentDescription = "Download state",
+            )
+            Column(
+                modifier = Modifier.padding(start = 4.dp)
+            ) {
+                Text(text = model.name)
+                if (model.id != LocalAiModel.CUSTOM.id) {
+                    Text(model.size)
+                }
+            }
+            Spacer(modifier = Modifier.weight(1f))
+            if (model.id != LocalAiModel.CUSTOM.id) {
+                Button(
+                    modifier = Modifier.padding(end = 8.dp),
+                    onClick = { onDownloadCardButtonClick(model) },
+                ) {
+                    Text(
+                        text = stringResource(
+                            id = when (model.downloadState) {
+                                is DownloadState.Downloading -> R.string.cancel
+                                is DownloadState.Error -> R.string.retry
+                                else -> {
+                                    if (model.downloaded) R.string.delete
+                                    else R.string.download
+                                }
+                            }
+                        ),
+                        color = LocalContentColor.current,
+                    )
+                }
+            }
+        }
+        if (model.id == LocalAiModel.CUSTOM.id) {
+            Column(
+                modifier = Modifier.padding(8.dp),
+            ) {
+                Text(
+                    text = stringResource(id = R.string.model_local_custom_title),
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+                Text(
+                    text = stringResource(id = R.string.model_local_custom_sub_title),
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+                Spacer(modifier = Modifier.height(4.dp))
+
+                fun folderModifier(treeNum: Int) = Modifier.padding(start = (treeNum - 1) * 12.dp)
+                val folderStyle = MaterialTheme.typography.bodySmall
+                Text(
+                    modifier = folderModifier(1),
+                    text = "Download",
+                    style = folderStyle,
+                )
+                Text(
+                    modifier = folderModifier(2),
+                    text = "SDAI",
+                    style = folderStyle,
+                )
+                Text(
+                    modifier = folderModifier(3),
+                    text = "model",
+                    style = folderStyle,
+                )
+
+                Text(
+                    modifier = folderModifier(4),
+                    text = "text_encoder",
+                    style = folderStyle,
+                )
+                Text(
+                    modifier = folderModifier(5),
+                    text = "model.ort",
+                    style = folderStyle,
+                )
+
+                Text(
+                    modifier = folderModifier(4),
+                    text = "tokenizer",
+                    style = folderStyle,
+                )
+                Text(
+                    modifier = folderModifier(5),
+                    text = "merges.txt",
+                    style = folderStyle,
+                )
+                Text(
+                    modifier = folderModifier(5),
+                    text = "special_tokens_map.json",
+                    style = folderStyle,
+                )
+                Text(
+                    modifier = folderModifier(5),
+                    text = "tokenizer_config.json",
+                    style = folderStyle,
+                )
+                Text(
+                    modifier = folderModifier(5),
+                    text = "tokenizer_config.json",
+                    style = folderStyle,
+                )
+
+                Text(
+                    modifier = folderModifier(4),
+                    text = "unet",
+                    style = folderStyle,
+                )
+                Text(
+                    modifier = folderModifier(5),
+                    text = "model.ort",
+                    style = folderStyle,
+                )
+
+                Text(
+                    modifier = folderModifier(4),
+                    text = "vae_decoder",
+                    style = folderStyle,
+                )
+                Text(
+                    modifier = folderModifier(5),
+                    text = "model.ort",
+                    style = folderStyle,
+                )
+            }
+        }
+        when (model.downloadState) {
+            is DownloadState.Downloading -> {
+                LinearProgressIndicator(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(),
+                    progress = model.downloadState.percent / 100f,
+                )
+            }
+            is DownloadState.Error -> {
+                Text(
+                    modifier = Modifier
+                        .padding(horizontal = 8.dp)
+                        .padding(bottom = 8.dp),
+                    text = stringResource(id = R.string.error_download_fail),
+                )
+            }
+            else -> Unit
+        }
     }
 }
 
