f13e5567d6b0a75f8b7a59839173212d22339d23
ShiftHackZ <dmitriy@moroz.cc>
2024-02-08 13:10:06 +0200

    Update fix

--- presentation/src/main/java/com/shifthackz/aisdv1/presentation/screen/setup/ServerSetupScreen.kt
+++ presentation/src/main/java/com/shifthackz/aisdv1/presentation/screen/setup/ServerSetupScreen.kt
@@ -482,6 +482,190 @@ private fun LocalDiffusionSetupTab(
     onSelectLocalModel: (ServerSetupState.LocalModel) -> Unit = {},
     onAllowLocalCustomModel: (Boolean) -> Unit = {},
 ) {
+    val modelItemUi: @Composable (ServerSetupState.LocalModel) -> Unit = { model ->
+        Column(
+            modifier = Modifier
+                .padding(vertical = 8.dp)
+                .fillMaxWidth()
+                .clip(RoundedCornerShape(16.dp))
+                .background(color = MaterialTheme.colorScheme.surfaceTint.copy(alpha = 0.8f))
+                .defaultMinSize(minHeight = 50.dp)
+                .border(
+                    width = 2.dp,
+                    shape = RoundedCornerShape(16.dp),
+                    color = if (model.selected) MaterialTheme.colorScheme.primary else Color.Transparent,
+                )
+                .clickable { onSelectLocalModel(model) },
+        ) {
+            Row(
+                modifier = Modifier.padding(vertical = 4.dp),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                val icon = when (model.downloadState) {
+                    is DownloadState.Downloading -> Icons.Outlined.FileDownload
+                    else -> when {
+                        model.id == LocalAiModel.CUSTOM.id -> Icons.Outlined.Landslide
+                        model.downloaded -> Icons.Outlined.FileDownloadDone
+                        else -> Icons.Outlined.FileDownloadOff
+                    }
+                }
+                Icon(
+                    modifier = modifier
+                        .padding(horizontal = 8.dp)
+                        .size(48.dp),
+                    imageVector = icon,
+                    contentDescription = "Download state",
+                )
+                Column(
+                    modifier = Modifier.padding(start = 4.dp)
+                ) {
+                    Text(text = model.name)
+                    if (model.id != LocalAiModel.CUSTOM.id) {
+                        Text(model.size)
+                    }
+                }
+                Spacer(modifier = Modifier.weight(1f))
+                if (model.id != LocalAiModel.CUSTOM.id) {
+                    Button(
+                        modifier = Modifier.padding(end = 8.dp),
+                        onClick = { onDownloadCardButtonClick(model) },
+                    ) {
+                        Text(
+                            text = stringResource(
+                                id = when (model.downloadState) {
+                                    is DownloadState.Downloading -> R.string.cancel
+                                    is DownloadState.Error -> R.string.retry
+                                    else -> {
+                                        if (model.downloaded) R.string.delete
+                                        else R.string.download
+                                    }
+                                }
+                            ),
+                            color = LocalContentColor.current,
+                        )
+                    }
+                }
+            }
+            if (model.id == LocalAiModel.CUSTOM.id) {
+                Column(
+                    modifier = Modifier.padding(8.dp),
+                ) {
+                    Text(
+                        text = stringResource(id = R.string.model_local_custom_title),
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = stringResource(id = R.string.model_local_custom_sub_title),
+                        style = MaterialTheme.typography.bodyMedium,
+                    )
+                    Spacer(modifier = Modifier.height(4.dp))
+
+                    fun folderModifier(treeNum: Int) =
+                        Modifier.padding(start = (treeNum - 1) * 12.dp)
+
+                    val folderStyle = MaterialTheme.typography.bodySmall
+                    Text(
+                        modifier = folderModifier(1),
+                        text = "Download",
+                        style = folderStyle,
+                    )
+                    Text(
+                        modifier = folderModifier(2),
+                        text = "SDAI",
+                        style = folderStyle,
+                    )
+                    Text(
+                        modifier = folderModifier(3),
+                        text = "model",
+                        style = folderStyle,
+                    )
+
+                    Text(
+                        modifier = folderModifier(4),
+                        text = "text_encoder",
+                        style = folderStyle,
+                    )
+                    Text(
+                        modifier = folderModifier(5),
+                        text = "model.ort",
+                        style = folderStyle,
+                    )
+
+                    Text(
+                        modifier = folderModifier(4),
+                        text = "tokenizer",
+                        style = folderStyle,
+                    )
+                    Text(
+                        modifier = folderModifier(5),
+                        text = "merges.txt",
+                        style = folderStyle,
+                    )
+                    Text(
+                        modifier = folderModifier(5),
+                        text = "special_tokens_map.json",
+                        style = folderStyle,
+                    )
+                    Text(
+                        modifier = folderModifier(5),
+                        text = "tokenizer_config.json",
+                        style = folderStyle,
+                    )
+                    Text(
+                        modifier = folderModifier(5),
+                        text = "tokenizer_config.json",
+                        style = folderStyle,
+                    )
+
+                    Text(
+                        modifier = folderModifier(4),
+                        text = "unet",
+                        style = folderStyle,
+                    )
+                    Text(
+                        modifier = folderModifier(5),
+                        text = "model.ort",
+                        style = folderStyle,
+                    )
+
+                    Text(
+                        modifier = folderModifier(4),
+                        text = "vae_decoder",
+                        style = folderStyle,
+                    )
+                    Text(
+                        modifier = folderModifier(5),
+                        text = "model.ort",
+                        style = folderStyle,
+                    )
+                }
+            }
+            when (model.downloadState) {
+                is DownloadState.Downloading -> {
+                    LinearProgressIndicator(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                        progress = model.downloadState.percent / 100f,
+                    )
+                }
+
+                is DownloadState.Error -> {
+                    Text(
+                        modifier = Modifier
+                            .padding(horizontal = 8.dp)
+                            .padding(bottom = 8.dp),
+                        text = stringResource(id = R.string.error_download_fail),
+                    )
+                }
+
+                else -> Unit
+            }
+        }
+    }
+
     Column(
         modifier = modifier.padding(horizontal = 16.dp),
     ) {
@@ -520,7 +704,9 @@ private fun LocalDiffusionSetupTab(
                 style = MaterialTheme.typography.bodyMedium,
             )
             OutlinedButton(
-                modifier = Modifier.fillMaxSize().padding(vertical = 8.dp),
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(vertical = 8.dp),
                 onClick = launchManageStoragePermission,
             ) {
                 Text(
@@ -534,13 +720,7 @@ private fun LocalDiffusionSetupTab(
                 val customPredicate = it.id == LocalAiModel.CUSTOM.id
                 if (state.localCustomModel) customPredicate else !customPredicate
             }
-            .forEach { localModel ->
-                LdModelComposable(
-                    model = localModel,
-                    onDownloadCardButtonClick = onDownloadCardButtonClick,
-                    onSelect = onSelectLocalModel,
-                )
-            }
+            .forEach { localModel -> modelItemUi(localModel) }
         Text(
             modifier = Modifier.padding(top = 16.dp),
             text = stringResource(id = R.string.hint_local_diffusion_warning),
@@ -594,192 +774,6 @@ private fun ConfigurationModeButton(
             textAlign = TextAlign.Center,
             lineHeight = 15.sp,
         )
-    }
-}
-
-@Composable
-fun LdModelComposable(
-    modifier: Modifier = Modifier,
-    model: ServerSetupState.LocalModel,
-    onDownloadCardButtonClick: (ServerSetupState.LocalModel) -> Unit = {},
-    onSelect: (ServerSetupState.LocalModel) -> Unit = {}
-) {
-    Column(
-        modifier = modifier
-            .padding(vertical = 8.dp)
-            .fillMaxWidth()
-            .clip(RoundedCornerShape(16.dp))
-            .background(color = MaterialTheme.colorScheme.surfaceTint.copy(alpha = 0.8f))
-            .defaultMinSize(minHeight = 50.dp)
-            .border(
-                width = 2.dp,
-                shape = RoundedCornerShape(16.dp),
-                color = if (model.selected) MaterialTheme.colorScheme.primary else Color.Transparent,
-            )
-            .clickable { onSelect(model) },
-    ) {
-        Row(
-            modifier = Modifier.padding(vertical = 4.dp),
-            horizontalArrangement = Arrangement.Center,
-            verticalAlignment = Alignment.CenterVertically,
-        ) {
-            val icon = when (model.downloadState) {
-                is DownloadState.Downloading -> Icons.Outlined.FileDownload
-                else -> when {
-                    model.id == LocalAiModel.CUSTOM.id -> Icons.Outlined.Landslide
-                    model.downloaded -> Icons.Outlined.FileDownloadDone
-                    else -> Icons.Outlined.FileDownloadOff
-                }
-            }
-            Icon(
-                modifier = modifier
-                    .padding(horizontal = 8.dp)
-                    .size(48.dp),
-                imageVector = icon,
-                contentDescription = "Download state",
-            )
-            Column(
-                modifier = Modifier.padding(start = 4.dp)
-            ) {
-                Text(text = model.name)
-                if (model.id != LocalAiModel.CUSTOM.id) {
-                    Text(model.size)
-                }
-            }
-            Spacer(modifier = Modifier.weight(1f))
-            if (model.id != LocalAiModel.CUSTOM.id) {
-                Button(
-                    modifier = Modifier.padding(end = 8.dp),
-                    onClick = { onDownloadCardButtonClick(model) },
-                ) {
-                    Text(
-                        text = stringResource(
-                            id = when (model.downloadState) {
-                                is DownloadState.Downloading -> R.string.cancel
-                                is DownloadState.Error -> R.string.retry
-                                else -> {
-                                    if (model.downloaded) R.string.delete
-                                    else R.string.download
-                                }
-                            }
-                        ),
-                        color = LocalContentColor.current,
-                    )
-                }
-            }
-        }
-        if (model.id == LocalAiModel.CUSTOM.id) {
-            Column(
-                modifier = Modifier.padding(8.dp),
-            ) {
-                Text(
-                    text = stringResource(id = R.string.model_local_custom_title),
-                    style = MaterialTheme.typography.bodyMedium,
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    text = stringResource(id = R.string.model_local_custom_sub_title),
-                    style = MaterialTheme.typography.bodyMedium,
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-
-                fun folderModifier(treeNum: Int) = Modifier.padding(start = (treeNum - 1) * 12.dp)
-                val folderStyle = MaterialTheme.typography.bodySmall
-                Text(
-                    modifier = folderModifier(1),
-                    text = "Download",
-                    style = folderStyle,
-                )
-                Text(
-                    modifier = folderModifier(2),
-                    text = "SDAI",
-                    style = folderStyle,
-                )
-                Text(
-                    modifier = folderModifier(3),
-                    text = "model",
-                    style = folderStyle,
-                )
-
-                Text(
-                    modifier = folderModifier(4),
-                    text = "text_encoder",
-                    style = folderStyle,
-                )
-                Text(
-                    modifier = folderModifier(5),
-                    text = "model.ort",
-                    style = folderStyle,
-                )
-
-                Text(
-                    modifier = folderModifier(4),
-                    text = "tokenizer",
-                    style = folderStyle,
-                )
-                Text(
-                    modifier = folderModifier(5),
-                    text = "merges.txt",
-                    style = folderStyle,
-                )
-                Text(
-                    modifier = folderModifier(5),
-                    text = "special_tokens_map.json",
-                    style = folderStyle,
-                )
-                Text(
-                    modifier = folderModifier(5),
-                    text = "tokenizer_config.json",
-                    style = folderStyle,
-                )
-                Text(
-                    modifier = folderModifier(5),
-                    text = "tokenizer_config.json",
-                    style = folderStyle,
-                )
-
-                Text(
-                    modifier = folderModifier(4),
-                    text = "unet",
-                    style = folderStyle,
-                )
-                Text(
-                    modifier = folderModifier(5),
-                    text = "model.ort",
-                    style = folderStyle,
-                )
-
-                Text(
-                    modifier = folderModifier(4),
-                    text = "vae_decoder",
-                    style = folderStyle,
-                )
-                Text(
-                    modifier = folderModifier(5),
-                    text = "model.ort",
-                    style = folderStyle,
-                )
-            }
-        }
-        when (model.downloadState) {
-            is DownloadState.Downloading -> {
-                LinearProgressIndicator(
-                    modifier = Modifier
-                        .padding(8.dp)
-                        .fillMaxWidth(),
-                    progress = model.downloadState.percent / 100f,
-                )
-            }
-            is DownloadState.Error -> {
-                Text(
-                    modifier = Modifier
-                        .padding(horizontal = 8.dp)
-                        .padding(bottom = 8.dp),
-                    text = stringResource(id = R.string.error_download_fail),
-                )
-            }
-            else -> Unit
-        }
     }
 }
 
