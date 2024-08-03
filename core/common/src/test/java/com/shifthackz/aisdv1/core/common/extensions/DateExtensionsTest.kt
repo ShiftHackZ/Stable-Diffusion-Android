963e4fc7aabc0ab2eeb489247c504286ee83c792
ShiftHackZ <dmitriy@moroz.cc>
2024-08-03 14:57:24 +0300

    Unit Testing | Patch 46

--- core/common/src/test/java/com/shifthackz/aisdv1/core/common/extensions/DateExtensionsTest.kt
+++ core/common/src/test/java/com/shifthackz/aisdv1/core/common/extensions/DateExtensionsTest.kt
@@ -7,49 +7,27 @@ import java.util.Date
 class DateExtensionsTest {
 
     companion object {
-        private val date1 = Date(894333955000) // 1998-05-05 05:05:55
-        private val date2 = Date(882137712000) // 1997-12-15 05:15:12
+        private val date = Date(894333955000) // 1998-05-05 05:05:55
     }
 
     @Test
     fun `given date 05_05_1998, then getRawDay, expected 5`() {
         val expected = 5
-        val actual = date1.getRawDay()
-        Assert.assertEquals(expected, actual)
-    }
-
-    @Test
-    fun `given date 12_15_1997, then getRawDay, expected 15`() {
-        val expected = 15
-        val actual = date2.getRawDay()
+        val actual = date.getRawDay()
         Assert.assertEquals(expected, actual)
     }
 
     @Test
     fun `given date 05_05_1998, then getRawMonth, expected 5`() {
         val expected = 5
-        val actual = date1.getRawMonth()
-        Assert.assertEquals(expected, actual)
-    }
-
-    @Test
-    fun `given date 12_15_1997, then getRawMonth, expected 12`() {
-        val expected = 12
-        val actual = date2.getRawMonth()
+        val actual = date.getRawMonth()
         Assert.assertEquals(expected, actual)
     }
 
     @Test
     fun `given date 05_05_1998, then getRawYear, expected 1998`() {
         val expected = 1998
-        val actual = date1.getRawYear()
-        Assert.assertEquals(expected, actual)
-    }
-
-    @Test
-    fun `given date 12_15_1997, then getRawYear, expected 1997`() {
-        val expected = 1997
-        val actual = date2.getRawYear()
+        val actual = date.getRawYear()
         Assert.assertEquals(expected, actual)
     }
 }
